4603c0c1a3860675e50316c9c149dee4c365a558
olahsimi <olahsimon26@gmail.com>
2024-11-28 13:44:49 +0100

    Delete deg_f2.m

--- deg_f2.m
+++
@@ -1,8 +0,0 @@
-function [fokszam, kapcsolat] = deg_f2(A, ember_num) %fokszám számító függvény
-    fokszam = sum(A);
-    kapcsolat={};
-   for i=1:1:ember_num
-       
-       kapcsolat{i} = find(A(i,:)==1);
-   end
-end
